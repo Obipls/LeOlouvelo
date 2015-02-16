e802dd8cffd23005a596988656b523ae440a12e2
Olivier Louwaars <o_louwaars@hotmail.com>
2015-02-16 18:58:25 +0100

    vlagfunctionaliteit toegevoegd, vlag verandert bij opnieuw selecteren land, aanpassen

--- flag_ui.py
+++ flag_ui.py
@@ -3,33 +3,42 @@
 # Olivier Louwaars s2814714 en Leonardo Losno Velozo s1668501
 # 16-2-2015
 
-from PyQt4 import QtCore, QtGui
-import sys
+
 from country import *
 
 class Flag(QtGui.QWidget):
     def __init__(self):
-        super(Flag, self).__init__()
+        super(Flag,self).__init__()
         self.initUI()
 
     def initUI(self):
 
-        self.box = QtGui.QComboBox(self)
+        self.box=QtGui.QComboBox(self)
+        self.box.activated.connect(self.onActivated)
 
-        hbox = QtGui.QHBoxLayout()
-        hbox.addWidget(self.box)
-        self.setLayout(hbox)
+        self.hbox=QtGui.QHBoxLayout()
+        self.hbox.addWidget(self.box)
+        self.setLayout(self.hbox)
 
-        for i in readText():
-            self.box.addItem(i.getName())
+        self.fbox=QtGui.QFrame(self)
+        self.fbox.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.hbox.addWidget(self.fbox)
+
+        for self.i in readText():
+            self.box.addItem(self.i.getName())
 
         self.resize(400, 100)
         self.center()
         self.setWindowTitle('Countries flag')
         self.show()
 
+
     def center(self):
-        qr = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr=self.frameGeometry()
+        cp=QtGui.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def onActivated(self, text):
+        self.flag=Country.getFlag(self.i)
+        self.fbox.setStyleSheet("QFrame{background-color:%s}"%self.flag.name())
